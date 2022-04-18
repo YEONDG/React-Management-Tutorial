b5ad3008ab3be7608fe7f21ae98642dc1c24dbec
YDG <zzmn1234@naver.com>
2022-04-18 22:56:05 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Customer from "./components/Custimer";
 
 function App() {
+  const customers = [
+    {
+      id: 1,
+      image: "https://placeimg.com/64/64/any",
+      name: "홍길동",
+      birthday: "961222",
+      gender: "남자",
+      job: "대학생",
+    },
+    {
+      id: 2,
+      image: "https://placeimg.com/64/64/1",
+      name: "연동근",
+      birthday: "921212",
+      gender: "남자",
+      job: "취준생",
+    },
+    {
+      id: 3,
+      image: "https://placeimg.com/64/64/2",
+      name: "데브리",
+      birthday: "980422",
+      gender: "여자",
+      job: "대학원생",
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        customers.map(c => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          )
+        })
+      }
     </div>
   );
 }

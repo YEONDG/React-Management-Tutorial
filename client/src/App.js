dd978c7c2f5996126852393dadd484ef190d0500
YDG <zzmn1234@naver.com>
2022-04-19 23:47:06 +0900

    nodejs 좀더공부

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,37 @@
-
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Customer from "./components/Customer";
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import styles from './App.module.css';
-
-
-
-  const customers = [
-    {
-      id: 1,
-      image: "https://placeimg.com/64/64/any",
-      name: "홍길동",
-      birthday: "961222",
-      gender: "남자",
-      job: "대학생",
-    },
-    {
-      id: 2,
-      image: "https://placeimg.com/64/64/1",
-      name: "연동근",
-      birthday: "921212",
-      gender: "남자",
-      job: "취준생",
-    },
-    {
-      id: 3,
-      image: "https://placeimg.com/64/64/2",
-      name: "데브리",
-      birthday: "980422",
-      gender: "여자",
-      job: "대학원생",
-    },
-  ];
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import styles from "./App.module.css";
 
 function App() {
- 
+  const [customers, setCustomers] = useState([]);
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {  
+    axios.get("http://localhost:5000/api/customers")
+    .then((response) => {
+      setCustomers(response.data)
+    })
+  }, []);
+  console.log(setCustomers)
+
+  // const stateRefresh = async () => {
+  //   setInterval(() => {
+  //     setProgress((prevProgress) =>
+  //       prevProgress >= 100 ? 0 : prevProgress + 1
+  //     );
+  //   }, 20);
+  //   const result = await axios.get("http://localhost:5000/api/customers");
+  //   setCustomers(result.data);
+  // };
+
   return (
     <Paper className={styles.root}>
       <Table className={styles.table}>
@@ -51,9 +43,21 @@ function App() {
           <TableCell>성별</TableCell>
           <TableCell>직업</TableCell>
         </TableHead>
-
         <TableBody>
-         {customers.map(c => {return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>)})}
+          {customers!=0 ? customers.map((c) => {
+            return (
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+                // stateRefresh={stateRefresh}
+              />
+            );
+          }) : "00"}
         </TableBody>
       </Table>
     </Paper>
